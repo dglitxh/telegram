79ab8b322159e9fce8908e51096a9196c261ac1d
YD <ydglitch@gmail.com>
2022-04-28 16:46:34 +0000

    quotes

--- telbot.py
+++ telbot.py
@@ -1,5 +1,8 @@
+import json
 import os
+import requests
 from dotenv import load_dotenv
+from setuptools import Command
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Updater,
@@ -17,17 +20,24 @@ updater = Updater(token)
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hello im Xenrix, type /help to see what i can do")
 
+def quote(update: Update, context: CallbackContext):
+    req = requests.get("https://zenquotes.io/api/random")
+    res = json.loads(req.text)[0]
+    msg = f"{res['q']}\n\n    ~ {res['a']}"
+    update.message.reply_text(msg)
+
 def _help(update: Update, context: CallbackContext):
     update.message.reply_text(
         """Available Commands :-
-    /youtube - To get the youtube URL
-    /linkedin - To get the LinkedIn profile URL
-    /gmail - To get gmail URL
-    /geeks - To get the GeeksforGeeks URL"""
+    /help - get help on how to use bot.
+    /quote - get an inspirational quote.
+    """
   
     )
 
 updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.dispatcher.add_handler(CommandHandler("help", _help))
+updater.dispatcher.add_handler(CommandHandler("quote", quote))
 
 updater.start_polling()
+print("We are live!!")
